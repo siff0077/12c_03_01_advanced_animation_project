92f8ef145700f2d2b4ae4bb0c619c248d4dd7fb4
Sara Nielsen <sara345a@stud.kea.dk>
2021-03-21 12:43:20 +0100

    responsive navbar created

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict"
+
+const menu = document.querySelector('#mobile_menu');
+const menuLinks = document.querySelector('.nav_menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+
+})
