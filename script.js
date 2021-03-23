36f4e273702e7eacc6c7460f7549f19ab413ff16
Sara Nielsen <sara345a@stud.kea.dk>
2021-03-23 11:56:35 +0100

    progress scroll animation done

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 "use strict"
+window.addEventListener("DOMContentLoaded", initPage);
+
+function initPage() {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to('progress', {
+    value: 100,
+    ease: 'none',
+    scrollTrigger: { scrub: 0.3 }
+  });
+
+
 
 const menu = document.querySelector('#mobile_menu');
 const menuLinks = document.querySelector('.nav_menu');
 
 menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
+menu.classList.toggle('is-active');
+menuLinks.classList.toggle('active');
 
 })
+}
+
+
+
+
+
+
