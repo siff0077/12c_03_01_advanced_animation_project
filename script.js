403f3046cef3e3163eda03dccf2ecb2e97be2128
Sara Nielsen <sara345a@stud.kea.dk>
2021-03-23 18:04:37 +0100

    scrollpage done and is WORKING

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ function initPage() {
 
 
 
+  gsap.registerPlugin(ScrollTrigger);
+        gsap.to("#box1",{x: "10vw", scrollTrigger:{trigger:"#box1",scrub:true}});
+        gsap.to("#box2",{x: "-10vw", scrollTrigger:{trigger:"#box2",scrub:true}});
+        gsap.to("#box3",{x: "10vw", scrollTrigger:{trigger:"#box3",scrub:true}});
+        gsap.to("#box4",{x: "-10vw", scrollTrigger:{trigger:"#box4",scrub:true}});
+        gsap.to("#box5",{x: "10vw", scrollTrigger:{trigger:"#box5",scrub:true}});
+        gsap.to("#box6",{x: "-10vw", scrollTrigger:{trigger:"#box6",scrub:true}});
+
+
+
+
+
 const menu = document.querySelector('#mobile_menu');
 const menuLinks = document.querySelector('.nav_menu');
 
